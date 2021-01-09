16b3620f00d085c34168523fe0b0a4669ca5637c
MP <47081376+mpiechulek@users.noreply.github.com>
2021-01-09 13:30:30 +0100

    removed cors

--- backend/app.js
+++ backend/app.js
@@ -39,19 +39,19 @@ app.use('/', express.static(path.join(__dirname, 'angular')));
 // multer for file sending 
 
 // CORS
-app.use((req, res, next) => {
+// app.use((req, res, next) => {
 
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader(
-    'Access-Control-Allow-Headers',
-    'Origin, X-Requested-Width, Content-Type, Accept, Authorization'
-  );
-  res.setHeader(
-    'Access-Control-Allow-Methods',
-    'GET, POST, PATCH, DELETE, PUT, OPTIONS'
-  );
-  next();
-});
+//   res.setHeader('Access-Control-Allow-Origin', '*');
+//   res.setHeader(
+//     'Access-Control-Allow-Headers',
+//     'Origin, X-Requested-Width, Content-Type, Accept, Authorization'
+//   );
+//   res.setHeader(
+//     'Access-Control-Allow-Methods',
+//     'GET, POST, PATCH, DELETE, PUT, OPTIONS'
+//   );
+//   next();
+// });
 
 // =============================================================================
 
