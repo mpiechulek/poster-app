e049d859a972653a12f8f80aefa7606cb9e8ca12
MP <47081376+mpiechulek@users.noreply.github.com>
2021-01-09 14:05:51 +0100

    croese trying toh fix

--- backend/app.js
+++ backend/app.js
@@ -33,25 +33,25 @@ app.use(bodyParser.urlencoded({
 }));
 
 //any request targeting /images will be allowed
-app.use('/images', express.static(path.join(__dirname, 'backend/images')));
+app.use('/images', express.static(path.join(__dirname, 'images')));
 app.use('/', express.static(path.join(__dirname, 'angular')));
 
 // multer for file sending 
 
 // CORS
-// app.use((req, res, next) => {
+app.use((req, res, next) => {
 
-//   res.setHeader('Access-Control-Allow-Origin', '*');
-//   res.setHeader(
-//     'Access-Control-Allow-Headers',
-//     'Origin, X-Requested-Width, Content-Type, Accept, Authorization'
-//   );
-//   res.setHeader(
-//     'Access-Control-Allow-Methods',
-//     'GET, POST, PATCH, DELETE, PUT, OPTIONS'
-//   );
-//   next();
-// });
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-Width, Content-Type, Accept, Authorization'
+  );
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'GET, POST, PATCH, DELETE, PUT, OPTIONS'
+  );
+  next();
+});
 
 // =============================================================================
 
